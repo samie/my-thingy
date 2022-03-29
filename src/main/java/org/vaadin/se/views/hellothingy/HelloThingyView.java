0aab02f33c9d6ef6182c57bb0428fd16d1fafc80
Sami Ekblad <sami@vaadin.com>
2022-03-29 08:18:42 +0300

    added test view

--- src/main/java/org/vaadin/se/views/hellothingy/HelloThingyView.java
+++ src/main/java/org/vaadin/se/views/hellothingy/HelloThingyView.java
@@ -1,12 +1,12 @@
 package org.vaadin.se.views.hellothingy;
 
+import com.vaadin.flow.component.DetachEvent;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
+import org.vaadin.se.Thingy52;
 import org.vaadin.se.views.MainLayout;
 
 @PageTitle("Hello Thingy")
@@ -14,20 +14,27 @@ import org.vaadin.se.views.MainLayout;
 @RouteAlias(value = "", layout = MainLayout.class)
 public class HelloThingyView extends HorizontalLayout {
 
-    private TextField name;
-    private Button sayHello;
+    private Button scanAndConnect, setLed;
+    private Thingy52 thingy;
 
     public HelloThingyView() {
-        name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
+        thingy = new Thingy52();
+        scanAndConnect = new Button("Scan and connect");
+        scanAndConnect.addClickListener(e -> {
+            thingy.scan();
         });
-
+        setLed = new Button("Light up!");
+        setLed.addClickListener(e -> {
+            thingy.setLED((int) (255*Math.random()),
+                    (int) (255*Math.random()),
+                    (int) (255*Math.random()));
+        });
         setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
-
-        add(name, sayHello);
+        add(scanAndConnect, setLed);
     }
 
+    @Override
+    protected void onDetach(DetachEvent detachEvent) {
+        thingy.disconnect();
+    }
 }
