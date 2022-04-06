c633a020a81428b7872450c898f141f56dc22662
Sami Ekblad <sami@vaadin.com>
2022-04-06 17:02:13 +0300

    Add debug listeners (still no server events)

--- frontend/globalThingy.js
+++ frontend/globalThingy.js
@@ -1,3 +1,16 @@
 /* Publish Thingy:52 API in global window variable. */
 import { Thingy } from './thingy52.js';
 window.Thingy = Thingy;
+
+
+const debug  = function(e) {
+  console.log(e);
+}
+
+window.Thingy.addEventListener("thingy52_connect",debug);
+
+window.Thingy.addEventListener("thingy52_battery",debug);
+
+window.Thingy.addEventListener("thingy52_disconnect",debug);
+
+window.Thingy.addEventListener("thingy52_button",debug);
