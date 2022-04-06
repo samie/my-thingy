92993bb8a60ab6d15510e433f00739877231bbd9
Sami Ekblad <sami@vaadin.com>
2022-04-06 17:02:32 +0300

    Added led API

--- src/main/java/org/vaadin/se/Thingy52.java
+++ src/main/java/org/vaadin/se/Thingy52.java
@@ -1,5 +1,6 @@
 package org.vaadin.se;
 
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.dependency.JsModule;
 
@@ -7,16 +8,39 @@ import com.vaadin.flow.component.dependency.JsModule;
 @JsModule("./globalThingy.js")
 public class Thingy52 {
 
+
+    /* Supported led modes.
+          0 = off, 1 = on, 2 = breathe, 3 = one shot
+        */
+    public enum LedMode {
+        OFF(0), 
+        ON(1),
+        BREATHE(2),
+        ONE_SHOT(3);
+
+        private final int value;
+
+        LedMode(int mode) {
+            this.value = mode;
+        }
+    }
+
+
     public void scan() {
         UI.getCurrent().getPage().executeJs("Thingy.scan()");
     }
 
     public void disconnect() {
-        UI.getCurrent().getPage().executeJs("Thingy.discussion()");
+        UI.getCurrent().getPage().executeJs("Thingy.disconnect()");
     }
 
-    public void setLED(int r, int g, int b) {
-        UI.getCurrent().getPage().executeJs("Thingy.setLED($0,$1,$2)", r, g, b);
+    public void setLed(int r, int g, int b) {
+        UI.getCurrent().getPage().executeJs("Thingy.setLED($0,$1,$2)",  r, g, b);
     }
+
+    public void setLedOff() {
+        UI.getCurrent().getPage().executeJs("Thingy.setLEDOff()");
+    }
+
 
 }
